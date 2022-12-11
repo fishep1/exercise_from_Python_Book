fd1bef7afadc0ea96a9e84ef7f3ae5e807606eae
Nikita Pletnev <betpezeh1@yahoo.com>
2022-12-11 13:00:37 +0500

    add exercise003

---
+++ exercise003/my_listens_group.py
@@ -0,0 +1,8 @@
+# names = ['Sergey', 'Nikolai', 'Maxim', 'Alexander']
+# print(f'Имя моего лучшего друга - {names[0].title()}')
+# print(f'Имя моего лучшего друга - {names[1].title()}')
+# print(f'Имя моего лучшего друга - {names[2].title()}')
+# print(f'Имя моего лучшего друга - {names[3].title()}')
+
+cars = ['Kia','BMV','mercedes','haval']
+print(f'я хотел бы купить автомобиль - {cars[0].title()} ')
